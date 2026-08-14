4907f2c201fc4175b9f42b87f8c9d4a184989483
INTIYAZ T <intiyazintiyaz14@gmail.com>
2026-08-14 16:06:13 +0530

    Update UserController.java

--- Project Folder/Main/controller/UserController.java
+++ Project Folder/Main/controller/UserController.java
@@ -1,1 +1,26 @@
+package com.example.razorpay.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.razorpay.entity.User;
+import com.example.razorpay.service.UserService;
+import com.razorpay.RazorpayException;
+
+@RestController
+@RequestMapping("/payments")
+public class UserController{
+
+	@Autowired
+	UserService userService;
+	
+	@PostMapping
+	public Object upay(@RequestBody User user) throws RazorpayException {
+		
+		return userService.payment(user.getAmount());
+	}
+}
 
