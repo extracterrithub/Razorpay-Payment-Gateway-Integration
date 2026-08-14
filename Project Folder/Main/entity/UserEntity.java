037e14de3ebba5657e2ca1ed712bbc7042c930d8
INTIYAZ T <intiyazintiyaz14@gmail.com>
2026-08-14 16:06:54 +0530

    Update UserEntity.java

--- Project Folder/Main/entity/UserEntity.java
+++ Project Folder/Main/entity/UserEntity.java
@@ -1,1 +1,20 @@
+package com.example.razorpay.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+	
+	private int amount;
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+
+}
 
